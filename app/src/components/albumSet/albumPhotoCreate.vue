474632616565c3f8af5dfe7d0af4325c473046b4
HaDev <ha.shownoob@gmail.com>
2024-02-29 18:17:54 +0700

    update

--- app/src/components/albumSet/albumPhotoCreate.vue
+++ app/src/components/albumSet/albumPhotoCreate.vue
@@ -18,6 +18,7 @@ const countryName = ref("");
 const cityName = ref("");
 const myStore = useAlbum();
 const cModel = ref([] as iAlbumFile [])
+const uploadedFiles = ref({});
 
 const actions = () => {
   const ac = {
@@ -78,6 +79,34 @@ const events = () => {
   };
   return ev;
 };
+
+const formatSize = (bytes: any) => {
+    if (bytes === 0) {
+        return '0 B';
+    }
+    let k = 1000, dm = 3, sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+const objUrl = (o: any) => {
+    return URL.createObjectURL(o);
+}
+
+const checkTypeFile = (str: string) => {
+    if (str == 'image/jpeg' || str == 'image/jpg' || str == 'image/png' || str == 'image/gif') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const onRemoveTemplatingFile = (file: any, removeFileCallback: Function, index: number) => {
+    // fileModel.value = fileModel.value.filter(fm => fm.albumFileName != file.name);
+    // countFilesSelected.value = countFilesSelected.value - 1;
+    removeFileCallback(index);
+    // totalSize.value -= parseInt(formatSize(file.size));
+    // totalSizePercent.value = totalSize.value / 10;
+}
 
 onMounted(async () => {
   await actions()
@@ -179,7 +208,70 @@ onMounted(async () => {
             <div class="box-title-sub"><h4><i class="fa-solid fa-circle-check title-icon"></i> ไฟล์แนบ</h4></div>
           </div>
           <div class="col-12 height-bottom">
-            <UploadFilesComponent v-model:model="cModel" />
+
+            <!-- <UploadFilesComponent v-model:model="cModel" /> -->
+            <FileUpload 
+                    name="demo[]" url="/api/upload" 
+                    @click="events().onAdvancedUpload($event)" 
+                    :multiple="true" 
+                    accept="image/*" 
+                    :maxFileSize="1000000"
+                    choose-label="เลือกรูป"
+                    cancel-label="ลบทั้งหมด"
+                    upload-label="อัปโหลด"
+                >
+            </FileUpload>
+
+            <!-- <FileUpload name="demo[]" url="/api/upload" :multiple="true" @upload="events().onAdvancedUpload($event)">
+              <template #header="{ chooseCallback, clearCallback, files }">
+                  <div class="row mt-0 mb-4">
+                      <div class="col"></div>
+                      <div class="col-auto">
+                          <button @click="chooseCallback()" class="btn-save p-button p-component p-button-sm p-button-rounded w-150 mt-4" type="button">
+                              <span class="p-button-label"><i class="pi pi-upload px-2"></i>เลือกไฟล์</span>
+                          </button>
+                      </div>
+                      <div class="col-auto">
+                          <button @click="clearCallback()" :disabled="files.length ==0" class="btn-cancel p-button p-component p-button-sm p-button-rounded p-button-danger w-150 mt-4" type="button">
+                              <span class="p-button-label"><i class="pi pi-undo px-2"></i>ลบไฟล์ทั้งหมด</span>
+                          </button>
+                      </div>
+                  </div>
+              </template>
+              <template #content="{ files, removeFileCallback }" >
+                  <div v-if="files.length > 0">
+                      <div v-for="(file, index) of files" :key="file.name + file.type + file.size" class="card m-0 px-6">
+                          <div class="row row-border-bottom">
+                              <div class="col-1">
+                                  <img role="presentation" :alt="file.name" :src="objUrl(file)" height="50" class="m-2 shadow-2"
+                                      v-if="checkTypeFile(file.type)" />
+                                  <img src="../../assets/images/icon-file.png" class="m-2" v-if="!checkTypeFile(file.type)"
+                                      height="50">
+                              </div>
+                              <div class="col-2 pt-3">
+                                  <div class="fc-green"><b>ขนาดไฟล์ :</b> {{ formatSize(file.size) }}</div>
+                              </div>
+                              <div class="col pt-3">
+                                  <div class="fc-green"><b>ชื่อไฟล์ :</b> {{ file.name }}</div>
+                              </div>
+                              <div class="col-2 align-items-right">
+                                  <Button icon="pi pi-times" v-tooltip.top="'ลบไฟล์'"
+                                      @click="onRemoveTemplatingFile(file, removeFileCallback, index)"
+                                      class="p-button-outlined p-button-danger p-button-rounded" />
+                              </div>
+                          </div>
+                      </div>
+                  </div> 
+                  <div class="align-items-center">
+                      <div class="row my-4">
+                          <div class="col">
+                              <i class="pi pi-cloud-upload p-2 fs-60 fc-green" />
+                              <p class="mt-2 mb-0 fc-green font-kanit">ลากไฟล์ลง ที่นี่ เพื่ออัพโหลด</p>
+                          </div>
+                      </div>
+                  </div>    
+              </template>
+          </FileUpload> -->
           </div>
         </div>
       </div>
@@ -418,5 +510,73 @@ onMounted(async () => {
 .data-disable {
     pointer-events: none;
     opacity: 0.5;
+}
+
+.mt-70 {
+    margin-top: -70px !important;
+    margin-bottom: 20px !important;
+}
+
+.w-150 {
+    width: 150px;
+}
+
+.row-border-top {
+    border-top: 1px solid var(--dlg-bd-color) !important;
+    padding-top: 10px;
+}
+
+.row-border-bottom {
+    border-bottom: 1px solid var(--dlg-bd-color) !important;
+    padding-top: 10px;
+}
+
+:global(.custom-progress-bar) {
+    .p-progressbar-value {
+        background-color: #f44336;
+    }
+}
+
+:global(.p-fileupload .p-fileupload-buttonbar) {
+    background: unset !important;
+    padding: unset !important;
+    border: unset !important;
+}
+
+:global(.p-fileupload .p-fileupload-content) {
+    background: unset !important;
+    border-top-right-radius: 6px;
+    border-top-left-radius: 6px;
+    padding: 15px 15px 10px 15px !important;
+    border: 1px solid var(--dlg-bd-color) !important;
+    color: var(--inactive-color);
+}
+
+:global(.p-fileupload .p-progressbar) {
+    height: 0.5rem !important;
+}
+
+:global(.p-fileupload-content) {
+    padding: 0rem !important;
+}
+
+.align-items-center {
+    text-align: center !important;
+}
+
+.align-items-right {
+    text-align: right !important;
+}
+
+.fs-60 {
+    font-size: 60px;
+}
+
+.fc-green {
+    color: var(--inactive-color);
+}
+
+.font-kanit {
+    font-family: "Kanit", sans-serif !important;
 }
 </style>
